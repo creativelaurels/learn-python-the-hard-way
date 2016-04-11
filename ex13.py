81b334b1fbd4cc30c892ba25ea90e94c4c837349
Lauren Farrell <lauren@creativelaurels.com>
2016-04-11 15:38:08 -0500

    Additional pratice

--- ex13.py
+++ ex13.py
@@ -6,3 +6,12 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+
+any_word = raw_input("Now you say anyword you would like to add: ")
+another_word = raw_input("Now give me another word: ")
+
+print "And I will cheer one more time:"
+
+print "You went with", once
+print "and also", twice
+print "but the best is", three
