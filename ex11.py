21f0efd9c5ee491cea5ab7d79370185813c066f4
Lauren Farrell <lauren@creativelaurels.com>
2016-04-11 10:53:16 -0500

    ex11.py

---
+++ ex11.py
@@ -0,0 +1,17 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % ( age, height, weight)
+
+print "Who is your favorite comic book character?",
+comic = raw_input()
+print "What is a location that is the size of the room you are in but is not a Starbucks or a bathroom?",
+location = raw_input()
+print "Name an occupation!"
+occupation = raw_input()
+
+print "Let's see %r, the %r, in a %r!" % (comic, occupation, location)
