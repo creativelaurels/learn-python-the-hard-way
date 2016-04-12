0c96460b6b9dfdf65dcff9e591be80e77a52b42c
Lauren Farrell <lauren@creativelaurels.com>
2016-04-12 14:16:15 -0500

    Created own version of ex19

---
+++ ex19-2.py
@@ -0,0 +1,19 @@
+def molly_treats(drybones_amt, chews_amt, vitamin_amt):
+	print "Molly has %d drybones left!" % drybones_amt
+	print "Molly has %d chews left!" % chews_amt
+	print "Molly has %d vitamins left!" % vitamin_amt
+	print "Gosh, she likes to eat!"
+	print "Good girl, Molly!\n"
+
+molly_treats(50, 5, 30)
+
+drybones = 10
+chews = 10
+vitamins = 5
+
+molly_treats(drybones, chews, vitamins)
+
+molly_treats(5 + 5, 3 + 2, 2 + 2)
+
+molly_treats(drybones * 10, chews - 5, vitamins / 3)
+
