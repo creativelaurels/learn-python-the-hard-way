cc47c939ee0a96869333645f6d16b4c6aabe0984
Lauren Farrell <lauren@creativelaurels.com>
2016-04-26 08:40:25 -0500

    ex37 testing

---
+++ ex37.py
@@ -0,0 +1,28 @@
+#ex37 - Testing out some things
+
+print "This is testing out some string escape sequences..."
+print "This is backslash: \\  end."
+print "This is single-quote: \'  end."
+print "This is double-quote: \"  end."
+print "This is bell: \a  end."
+print "This is backspace: \b  end."
+print "This is formfeed:  \f  end."
+print "This is newline: \n  end."
+print "This is carriage: \r  end."
+print "This is tab: \t  end."
+print "This is vertical tab: \v  end."
+
+
+print "This is formfeed:  \f  end."
+print "This is formfeed:  \f  end."
+print "This is formfeed:  \f  end."
+print "Test one two\ftest one two \ftest one two."
+
+print "This is vertical tab: \v  end."
+print "Test one two\vtest one two \vtest one two."
+
+print "This is carriage: \r  end."
+print "Test one two\rtest one two \rtest one two."
+
+print "This is backspace: \b  end."
+print "Test one two\btest one two \btest one two."
